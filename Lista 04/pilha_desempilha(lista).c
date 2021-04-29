18f0ed9eace73450e015ac9eb0bcbbe139666e52
Felipe Chermont <chernox0427@gmail.com>
2021-04-29 16:03:14 -0400

    Adicionando a formativa 04

---
+++ Lista 04/pilha_desempilha(lista).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct celula {
+  int dado;
+  struct celula *prox;
+} celula;
+
+
+int desempilha (celula *p, int *y) {
+  celula *lixo = p -> prox;
+  if (lixo == NULL) return 0;
+  p -> prox = lixo -> prox;
+  *y = lixo -> dado;
+  free(lixo);
+  return 1;
+}
+
+int desempilha (celula *p, int *y);
---
+++ Lista 04/pilha_empilha(vetor).c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct pilha {
+  int *dados;
+  int N, topo;
+} pilha;
+
+int empilha(pilha *p, int x) {
+  if(p != NULL){
+    if(p -> topo == p -> N -1) {
+      p -> dados = realloc(p -> dados, 2*p->N * sizeof(int));
+      if (p -> dados == NULL) return 0;
+      p -> N *= 2; 
+    } 
+    p -> dados[p->topo] = x;
+    p -> topo ++;
+    return 1;
+  }
+}
+
+int empilha(pilha *p, int x);
